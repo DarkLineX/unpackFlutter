67e7154e786db17b58cd5dc264644910759f2cde
MiDuoKi <105914203+MiDuoKi@users.noreply.github.com>
2022-08-11 17:56:57 +0800

    - fix code

--- dart_runtime/Constants.py
+++
@@ -1,8 +0,0 @@
-
-AOTSymbolsNameList= [
-    '_kDartVmSnapshotData',
-    '_kDartVmSnapshotInstructions',
-    '_kDartIsolateSnapshotData',
-    '_kDartIsolateSnapshotInstructions',
-    # '_kDartSnapshotBuildId'
-]
